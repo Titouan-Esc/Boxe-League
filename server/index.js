b161d4730a1419297df158da6283f58d57d61722
Mes Morts <titouanescorneboueu@gmail.com>
2021-04-09 16:05:57 +0200

    Début du back

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/user.route');
+const PORT = process.env.PORT || 8000;
+
+require('dotenv/config');
+
+
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser : true, useUnifiedTopology : true
+}, () => {
+    return console.log('Connecté à MongoDB')
+})
+
+
+
+
+const app = express();
+
+app.use(cookieParser());
+
+app.use(cors({
+    credentials : true,
+    origin : ['http://localhost:8000', 'http://localhost:3000']
+}));
+
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
