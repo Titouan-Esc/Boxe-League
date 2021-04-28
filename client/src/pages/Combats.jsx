481ab63c5c20975449756c65d2959002678029c6
Mes Morts <titouanescorneboueu@gmail.com>
2021-04-28 10:16:51 +0200

    Fin de la matinée

--- client/src/pages/Combats.jsx
+++ client/src/pages/Combats.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Link, Redirect } from 'react-router-dom';
+import { Link, Redirect, useHistory } from 'react-router-dom';
 import axios from 'axios';
-import CardsFight from '../components/CardsFight';
 
 const url = 'http://localhost:8000/api/cards';
 
@@ -9,6 +8,11 @@ const Combats = () => {
 
     const [cards, setCards] = useState([]);
     const [combat, setCombat] = useState(false);
+    const [checked, setChecked] = useState('');
+
+    let history = useHistory();
+
+    console.log(checked);
 
     async function fetchCards() {
         try {
@@ -18,6 +22,12 @@ const Combats = () => {
         } catch (error) {
             console.log(error)
         }
+        
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+
         
     }
 
@@ -47,11 +57,41 @@ const Combats = () => {
             <div className="middel_combat">
                 <h2>Combats</h2>
                 <p>Sélectionner vos combattans</p>
-                <button className='btn_fight'>combattez !</button>
             </div>
 
             <div className="select_champions">
-                <CardsFight cards={cards}/>
+                <form className='form-combat' onSubmit={submit}>
+                    <div className="form_top">
+                        <button className='btn_fight' type='submit'>combattez !</button>
+                    </div>
+                    <div className="form_bottom">
+                    {cards.map((c) => {
+                        const { _id, name, description, image, atk, def } = c;
+                        return(
+                            <div className="conteneur_carte" key={_id}>
+                                <form className='check_form'>
+                                    <div className="fond_carte">
+                                        <div className="carte">
+                                            <h2>{name}</h2>
+                                            <div className="carte_img">
+                                                <img src={image} alt="Image du champion"/>
+                                            </div>
+                                            <div className="carte_desc">
+                                                <p>{description}</p>
+                                            </div>
+                                            <div className="atk_def">
+                                                <p>ATK {atk}</p>
+                                                <p>DEF {def}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <input type="checkbox" onChange={() => setChecked(c._id)}/>
+                                </form>
+                            </div>
+                        )
+                    })}  
+                    </div>
+                </form>
             </div>
         </main>
     )
